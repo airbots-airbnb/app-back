d22a337fe472360e3ad6c28c01974e6f017ef885
cesar.lara@blueoceantech.com.mx <cesar.lara@blueoceantech.com.mx>
2019-07-17 20:26:37 -0500

    Cambios menores en comentarios

--- app/models/comentario.js
+++ app/models/comentario.js
@@ -1,4 +1,4 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const ComentarioSchema = new Schema(
@@ -8,14 +8,14 @@ const ComentarioSchema = new Schema(
 		},
 		alojamiento: {
 			type: Schema.Types.ObjectId,
-			ref: "alojamientos"
+			ref: 'alojamientos'
 		},
 		usuario: {
 			type: Schema.Types.ObjectId,
-			ref: "usuario"
+			ref: 'usuario'
 		}
 	},
-	{ collection: "comentario", timestamps: true }
+	{ collection: 'comentario', timestamps: true }
 );
 
-module.exports = mongoose.model("comentario", ComentarioSchema);
+module.exports = mongoose.model('comentario', ComentarioSchema);

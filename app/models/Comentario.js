5e6b469e97eff185f21ae31167e81f118d72bed6
cesar.lara@blueoceantech.com.mx <cesar.lara@blueoceantech.com.mx>
2019-07-18 19:30:42 -0500

    Se agrega comentario.

---
+++ app/models/Comentario.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const ComentarioSchema = new Schema(
+	{
+		comentario: {
+			type: String
+		},
+		alojamiento: {
+			type: Schema.Types.ObjectId,
+			ref: 'alojamientos'
+		},
+		usuario: {
+			type: Schema.Types.ObjectId,
+			ref: 'usuario' 
+		}
+	},
+	{ collection: 'comentario', timestamps: true }
+);
+
+module.exports = mongoose.model('comentario', ComentarioSchema);
